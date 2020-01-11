f1e90b02ed20ecb397e65d0fa0e99b32b0f3bc78
Andrew Ross <andrew.ross.mail@gmail.com>
2020-01-10 19:08:03 -0500

    Add routes to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Repo } from './repo';
+import { RepoService } from './services/repo.service';
+import { RepoIframeComponent } from './components/repo-iframe/repo-iframe.component';
 
+// get all the repo objects defined in 'repo-manifest'
+const repoService: RepoService = new RepoService;
+const repos: Repo[] = repoService.getRepos();
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', redirectTo: '/' + repos[0].name, pathMatch: 'full' },];
+
+// add a route for each repo object defined in 'repo-manifest'
+repos.forEach((repo) => {
+  routes.push({
+    path: repo.name,
+    component: RepoIframeComponent,
+    data: repo
+  })
+})
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
