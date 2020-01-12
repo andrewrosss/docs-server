befe4ee3e712e5da9e24694c728cd75b936109f4
Andrew Ross <andrew.ross.mail@gmail.com>
2020-01-12 12:24:20 -0500

    Add route handler for undefined end-points

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { Repo } from './repo';
 import { RepoService } from './services/repo.service';
 import { RepoIframeComponent } from './components/repo-iframe/repo-iframe.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 // get all the repo objects defined in 'repo-manifest'
 const repoService: RepoService = new RepoService;
 const repos: Repo[] = repoService.getRepos();
 
-const routes: Routes = [{ path: '', redirectTo: '/' + repos[0].name, pathMatch: 'full' },];
-
+// add the root end-point
+const routes: Routes = [{ path: '', redirectTo: `/${repos[0].name}`, pathMatch: 'full' }];
 // add a route for each repo object defined in 'repo-manifest'
 repos.forEach((repo) => {
   routes.push({
@@ -18,6 +19,8 @@ repos.forEach((repo) => {
     data: repo
   })
 })
+// all other routes go to the page-not-found component
+routes.push({ path: '**', component: PageNotFoundComponent })
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
