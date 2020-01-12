03fc830f7897aa5c0aa06639a02c18fe7bf79322
Andrew Ross <andrew.ross.mail@gmail.com>
2020-01-12 16:28:32 -0500

    Add site-config.service

---
+++ src/app/services/site-config.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SiteConfigService {
+
+  public toolbarTitle: string = 'Andrewrosss';
+
+  constructor() { }
+}
